ed64028936d6cadf237d028951f9e8a05c7341c0
Joseph <josemukorivo@gmail.com>
2019-10-23 17:12:18 +0200

    Step 4 v3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -13,6 +13,6 @@ export class ProductService {
   getAlbum(id: number): Observable<Album> {
     return this._http
       .get(this._albumUrl)
-      .map(response => response.json() as Album);
+      .map(response => <Album>response.json());
   }
 }
